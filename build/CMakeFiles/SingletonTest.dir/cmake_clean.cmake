d599ceedf3d36fa5352a5292ca03551d21200afc
vadimkriv <polenko1029@gmail.com>
2024-05-20 21:39:31 +0300

    a

---
+++ build/CMakeFiles/SingletonTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingletonTest.dir/tests/test_Singleton.cpp.o"
+  "CMakeFiles/SingletonTest.dir/tests/test_Singleton.cpp.o.d"
+  "SingletonTest"
+  "SingletonTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SingletonTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vadim/rk2
+# Build directory: /home/vadim/rk2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SingletonTest "/home/vadim/rk2/build/SingletonTest")
+set_tests_properties(SingletonTest PROPERTIES  _BACKTRACE_TRIPLES "/home/vadim/rk2/CMakeLists.txt;20;add_test;/home/vadim/rk2/CMakeLists.txt;0;")
