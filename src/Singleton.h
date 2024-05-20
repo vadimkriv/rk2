2683772cd7880f4bc51c29b77c7857005015cd29
vadimkriv <polenko1029@gmail.com>
2024-05-20 21:51:40 +0300

    Update Singleton.h

--- src/Singleton.h
+++ src/Singleton.h
@@ -1,4 +1,9 @@
-#include<atomic>
+
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+#include <iostream>
+#include <atomic>
 #include <mutex>
 
 class Singleton
@@ -8,13 +13,14 @@ public:
     void performTask() const;
     void reset();
 
-    // Delete the copy constructor and assingment operator
+    // Удаляем конструктор копирования и оператор присваивания
     Singleton(const Singleton &singleton) = delete;
     Singleton &operator = (const Singleton &singleton) = delete;
 
 private:
-    Singleton(){}
-    static std::atomic<Singleton *>instance;
+    Singleton() {}
+    static std::atomic<Singleton *> instance;
     static std::mutex mutex;
 };
 
+#endif // SINGLETON_H
