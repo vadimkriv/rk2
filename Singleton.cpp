5cd0f0509c1f7d01536740fc1ac9d7665ed186fe
vadimkriv <polenko1029@gmail.com>
2024-05-20 21:55:35 +0300

    Create Singleton.cpp

---
+++ Singleton.cpp
@@ -0,0 +1,29 @@
+#include "Singleton.h"
+
+std::atomic<Singleton *> Singleton::instance{nullptr};
+std::mutex Singleton::mutex;
+
+Singleton *Singleton::getInstance()
+{
+    if (!instance)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        if (!instance)
+        {
+            instance = new Singleton();
+        }
+    }
+    return instance;
+}
+
+void Singleton::performTask() const
+{
+    std::cout << "Performing some task" << std::endl;
+}
+
+void Singleton::reset()
+{
+    Singleton *value = instance.load();
+    delete value;
+    instance = nullptr;
+}
