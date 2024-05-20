0c6efa0e60f2c11b8e8d6a97bfcc457282ec8a14
vadimkriv <polenko1029@gmail.com>
2024-05-20 21:52:18 +0300

    Update test_Singleton.cpp

--- tests/test_Singleton.cpp
+++ tests/test_Singleton.cpp
@@ -1,24 +1,7 @@
+
 #include <iostream>
-#include <sstream> 
-#include<atomic>
-#include <mutex>
-
-class Singleton
-{
-public:
-    static Singleton *getInstance();
-    void performTask() const;
-    void reset();
-
-    // Delete the copy constructor and assingment operator
-    Singleton(const Singleton &singleton) = delete;
-    Singleton &operator = (const Singleton &singleton) = delete;
-
-private:
-    Singleton(){}
-    static std::atomic<Singleton *>instance;
-    static std::mutex mutex;
-};
+#include <sstream>
+#include "Singleton.h"
 
 void testInstanceNotNull() {
     Singleton* instance = Singleton::getInstance();
